dce4676022ff1ba71f7d37a2ce35f44734bc9962
yuto-kamimura <S6156@enecom-th.jp>
2024-05-23 09:19:47 +0000

    修正

--- test/reversePoland.java
+++ test/reversePoland.java
@@ -6,10 +6,9 @@ import java.util.List;
 public class reversePoland {
 
     public static void main(String[] args) {
-        String[] tests = { "5", "4", "4", "+", "-" };
         Stack stack = new Stack();
         Double val1, val2;
-        for (String arg : tests) {
+        for (String arg : args) {
             /*
              * 演算子の場合は、スタックから前二つの値を取り出し、計算結果をスタックに保存
              * 数値の場合は、String型をDouble型に直してスタックに保存
@@ -19,7 +18,7 @@ public class reversePoland {
                 case "+":
                     val1 = stack.pop();
                     val2 = stack.pop();
-                    stack.push(val2 + val1);
+                    stack.push(val2 + val1); 
                     break;
                 case "-":
                     val1 = stack.pop();
@@ -51,13 +50,13 @@ class Stack {
     List<Double> list = new ArrayList<Double>();
 
     void push(Double num) {
-        list.add(list.size(), num);
+        list.add(list.size(), num); // スタック(リスト)の一番後ろに数値を追加する。
     }
 
     Double pop() {
-        Double val = list.get(list.size() - 1);
-        list.remove(list.size()-1);
-        return val;
+        Double val = list.get(list.size() - 1); // スタックの一番最後の数値を取り出す
+        list.remove(list.size()-1); // 取り出した箇所のリスト要素は削除する
+        return val; // 取り出した値を返す
     }
 
 }
